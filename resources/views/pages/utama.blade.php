ee3f446fa73e0c070e0147ea4a6267ebf0da5c85
Nurul Nadhrah Mohamad Pauzi <nadhrah@PNSB.local>
2025-09-10 17:33:09 +0800

    changes

--- resources/views/pages/utama.blade.php
+++ resources/views/pages/utama.blade.php
@@ -4,5 +4,5 @@
     <h1>Utama</h1>
 
     <p>Nadhrah seorang yang rajin belajar dan dia hampir mendapatkan selempang pink selepas menyempurnakan 
-       internship dia di syarikat puncak niaga, semoga selempang pink miliknya!</p>
+       internship di syarikat puncak niaga, semoga selempang pink miliknya!</p>
 @endsection
